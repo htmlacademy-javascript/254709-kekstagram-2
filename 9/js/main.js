5178c73f64e98e5b3a09749f09f773c16dfa9f7c
Keks <keks@htmlacademy.ru>
2024-12-09 23:16:10 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,5 @@
+import {
+  renderGallery
+} from './thumbnails.js';
+
+renderGallery();
---
+++ 9/js/thumbnails.js
@@ -0,0 +1,28 @@
+import {
+  generateArrObj
+} from './data.js';
+
+const renderGallery = () => {
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('a');
+
+const userPictures = generateArrObj();
+const pictureFragment = document.createDocumentFragment();
+
+userPictures.forEach(({id, url, description, likes, comments}) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__img').alt = description;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureFragment.append(pictureElement);
+});
+
+picturesContainer.append(pictureFragment);
+};
+
+export {
+  renderGallery
+};
