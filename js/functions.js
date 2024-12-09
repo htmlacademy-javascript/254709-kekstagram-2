13685c7bbcfc3fec0c1ac90873685b0080c3611e
Влад <v1ad@inbox.ru>
2024-12-09 15:13:11 +0300

    Переделал логику

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,13 @@
-const getTime = (timeString) => [Number(timeString.split(':')[0]), Number(timeString.split(':')[1])];
-
 const isWithinWorkingHours = (workdayStart, workdayEnd, meetingStart, meetingLength) => {
-
-  const workdayStartTime = getTime(workdayStart);
-  const workdayEndTime = getTime(workdayEnd);
-  const meetingStartTime = getTime(meetingStart);
-  const meetingDuration = [Math.floor(meetingLength / 60), meetingLength - Math.floor(meetingLength / 60) * 60]
-  const meetingEndTime = [meetingStartTime[0] + meetingDuration[0], meetingStartTime[1] + meetingDuration[1]]
-  if (meetingEndTime[1] >= 60) {
-    meetingEndTime[0] += 1;
-    meetingEndTime[1] -= 60;
+  const convertToMinutes = (timeString) => {
+      const [hours, minutes] = timeString.split(':').map(Number);
+      return hours * 60 + minutes;
   };
-  if (
-    (workdayStartTime[0] > meetingStartTime[0]) ||
-    (workdayStartTime[0] === meetingStartTime[0] && workdayStartTime[1] > meetingStartTime[1]) ||
-    (workdayEndTime[0] < meetingEndTime[0]) ||
-    (workdayEndTime[0] === meetingEndTime[0] && workdayEndTime[1] < meetingEndTime[1])) {
-    return false
-  };
-  return true;
+  const workdayStartMinutes = convertToMinutes(workdayStart);
+  const workdayEndMinutes = convertToMinutes(workdayEnd);
+  const meetingStartMinutes = convertToMinutes(meetingStart);
+  const meetingEndMinutes = meetingStartMinutes + meetingLength;
+  return meetingStartMinutes >= workdayStartMinutes && meetingEndMinutes <= workdayEndMinutes
 };
 
 console.log(isWithinWorkingHours('08:00', '17:30', '14:00', 90));
