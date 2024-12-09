0754536cb2cbf2e07a91c82ac1d6ae4fbb6be4af
Влад <v1ad@inbox.ru>
2024-12-09 14:43:43 +0300

    Переделал функции

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,21 @@
-const getHours = (timeString) => Number(timeString.split(':')[0]);
-const getMinutes = (timeString) => Number(timeString.split(':')[1]);
+const getTime = (timeString) => [Number(timeString.split(':')[0]), Number(timeString.split(':')[1])];
 
 const isWithinWorkingHours = (workdayStart, workdayEnd, meetingStart, meetingLength) => {
 
-  // Получаем часы и минуты начала рабочего дня
-  let workdayStartHours = getHours(workdayStart);
-  let workdayStartMinutes = getMinutes(workdayStart);
-
-  // Получаем часы и минуты конца рабочего дня
-  let workdayEndHours = getHours(workdayEnd);
-  let workdayEndMinutes = getMinutes(workdayEnd);
-
-// Получаем часы и минуты начала встречи
-  let meetingStartHours = getHours(meetingStart);
-  let meetingStartMinutes = getMinutes(meetingStart);
-
-// Получаем часы и минуты конца встречи
-  let meetingEndHours = meetingStartHours + Math.floor(meetingLength / 60);
-  let meetingEndMinutes = meetingStartMinutes + (meetingLength - meetingEndHours * 60);
-  if (meetingEndMinutes >= 60) {
-    meetingEndHours += 1;
-    meetingEndMinutes -= 60;
+  const workdayStartTime = getTime(workdayStart);
+  const workdayEndTime = getTime(workdayEnd);
+  const meetingStartTime = getTime(meetingStart);
+  const meetingDuration = [Math.floor(meetingLength / 60), meetingLength - Math.floor(meetingLength / 60) * 60]
+  const meetingEndTime = [meetingStartTime[0] + meetingDuration[0], meetingStartTime[1] + meetingDuration[1]]
+  if (meetingEndTime[1] >= 60) {
+    meetingEndTime[0] += 1;
+    meetingEndTime[1] -= 60;
   };
-
-  // Проверяем начало и конец встречи на выход за рабочий день
-  if ((workdayStartHours > meetingStartHours) || (workdayStartHours === meetingStartHours && workdayStartMinutes > meetingStartMinutes) || (workdayEndHours < meetingEndHours) || (workdayEndHours === meetingEndHours && workdayEndMinutes < meetingEndMinutes)) {
+  if (
+    (workdayStartTime[0] > meetingStartTime[0]) ||
+    (workdayStartTime[0] === meetingStartTime[0] && workdayStartTime[1] > meetingStartTime[1]) ||
+    (workdayEndTime[0] < meetingEndTime[0]) ||
+    (workdayEndTime[0] === meetingEndTime[0] && workdayEndTime[1] < meetingEndTime[1])) {
     return false
   };
   return true;
